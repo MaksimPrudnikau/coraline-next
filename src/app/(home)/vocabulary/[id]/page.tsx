e5243bd69646eb0d02046f7e4f780599c0b38dbe
maksim.prudnikau <maksim.prudnikau@devstream,by>
2023-06-21 19:09:05 +0300

    try add vocabulary

--- src/app/(home)/vocabulary/[id]/page.tsx
+++ src/app/(home)/vocabulary/[id]/page.tsx
@@ -6,12 +6,38 @@ interface IRouteProps {
 
 export default async function Vocabulary(props: IDynamicRoute<IRouteProps>) {
     return (
-        <div className={'h-screen w-screen text-center'}>
-            <div className={'mt-10 mb-10 text-4xl font-bold'}>
+        <div className={'h-screen w-screen flex flex-col items-center'}>
+            <div className={'mt-10 mb-10 text-4xl font-bold text-'}>
                 Vocabulary Name
             </div>
-            <div className={'w-full'}>
-                <input placeholder={'Note'}/>
+            <div className={'min-w-[20%] min-h-[10%] max-w-prose'}>
+                <input className={'w-full h-full border-2 border-[#8098F9]/50 rounded-[10px] bg-[#D9D9D9] flex justify-center'} placeholder={'Note'} type={'text'}/>
             </div>
+                <div className={'w-[80%] h-fit text-center flex flex-col justify-center'}>
+                    <div className={'flex'}>
+                        <div>
+                            #
+                        </div>
+                        <div className={'grow'}>
+                            English
+                        </div>
+                        <div className={'grow'}>
+                            Japanese
+                        </div>
+                    </div>
+
+                    <div className={'flex'}>
+                        <div>
+                            1
+                        </div>
+                        <div className={'grow'}>
+                            <input className={'w-full'} type={'text'} value={'Asd'}/>
+                        </div>
+                        <div className={'grow'}>
+                            <input className={'w-full'} type={'text'} value={'Asd'}/>
+                        </div>
+                    </div>
+
+                </div>
         </div>)
 }
